8f659b1bbc4ea4a78c70e2fecd7ca9afc4b919c9
neskech <neskechastro@gmail.com>
2024-07-04 15:00:21 -0400

    undo

--- src/drawingEditor/Input/gesturesAndShortcuts/gestures/undoGesture.ts
+++ src/drawingEditor/Input/gesturesAndShortcuts/gestures/undoGesture.ts
@@ -32,7 +32,7 @@ export default class UndoGesture extends Gesture {
     const now = new Date().getTime();
     if (now - this.lastTapTime <= TAP_DELAY_MILLIS) {
       this.tapCount += 1;
-      if (this.tapCount == 1) {
+      if (this.tapCount == 2) {
         EventManager.invokeVoid('undo');
         this.tapCount = 0;
       }
